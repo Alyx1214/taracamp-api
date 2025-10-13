c7a711718c508f77761cadde08c32328be3e4715
MA. ANGELA SHANE USI <usishanelobo@gmail.com>
2025-10-13 13:19:54 +0800

    fix: correct typo in ServicesRates component pricing details

--- client/Guest/my-auth-app/src/components/MainServices/ServicesRates.jsx
+++ client/Guest/my-auth-app/src/components/MainServices/ServicesRates.jsx
@@ -7,7 +7,7 @@ function MainServicesRates() {
       <h2 className={styles.excessRatesTitle}>RATES IN EXCESS OF CAPACITY</h2>
       <div className={styles.excessRatesContent}>
         <p>₱200.00/pax with no provision of beddings/toiletries + 10% service fee</p>
-        <p>₱270.00/pax with with complete beddings/toiletries + 10% service fee</p>
+        <p>₱270.00/pax with complete beddings/toiletries + 10% service fee</p>
         <p>₱50.00/pax for conference halls + 10% service fee</p>
       </div>
     </section>
