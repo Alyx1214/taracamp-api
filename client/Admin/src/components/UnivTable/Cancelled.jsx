28305b88869c2cee770fc9323d0794a2fc119a6d
Khianna <2232337@slu.edu.ph>
2025-10-19 02:12:07 +0800

    Facility Name Added In Reservation

--- client/Admin/src/components/UnivTable/Cancelled.jsx
+++ client/Admin/src/components/UnivTable/Cancelled.jsx
@@ -36,8 +36,7 @@ export default function Cancelled({
   const [totalItems, setTotalItems] = useState(parentTotalItems);
 
   const itemsPerPage = 15;
-  const columns = useMemo(() => ["Name", "Email", "Service Type", "Date", "Actions"], []);
-
+  const columns = useMemo(() => ["Name", "Email", "Service Type", "Facility Name", "Date", "Actions"], []);
   useEffect(() => {
     let cancelled = false;
     async function fetchCancelled() {
