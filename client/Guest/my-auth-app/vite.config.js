efb822282c968a3d0220f1aa771b969bafc282c0
ALYSSA MARI ALBARDA <alyssa.albarda.1412@gmail.com>
2025-11-14 15:06:18 +0800

    changed to server url

--- client/Guest/my-auth-app/vite.config.js
+++ client/Guest/my-auth-app/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://taracamp-api.onrender.com',
+        target: 'https://taracamp-api.azurewebsites.net',
         //target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
